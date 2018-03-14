cb35aaa2f83580b7221c2b1fcf35dc1e2600d830
cavidan666666 <cavidan666666@gmail.com>
2018-03-15 02:40:59 +0400

    Cavcik commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace vovel_Constant
 {
@@ -10,6 +11,164 @@ namespace vovel_Constant
     {
         static void Main(string[] args)
         {
+            vovelConst voCo = new vovelConst();
+            Console.WriteLine("Cumlenizi Yazin");
+            var screenWord = Console.ReadLine();
+            voCo.Vowel(screenWord);
+
+
+
+
+
+            //void findDigitOfNumber()
+            //{
+
+
+            //    int i = 0;
+
+            //    Console.WriteLine("Istediyiniz Ededi Yazin");
+            //    var eded = Convert.ToInt32(Console.ReadLine());
+
+
+            //    do
+            //    {
+            //        eded = eded / 10;
+            //        i++;
+
+            //    } while (eded > 0);
+            //    Console.WriteLine("Reqemlerin Sayi " + i);
+
+            //}
+            //findDigitOfNumber();
+
+
+
+
+
+            //void findDigitOfNumberWithWhile()
+            //{
+            //    int i = 0;
+
+            //    Console.WriteLine("Istediyiniz Ededi Yazin");
+            //    var eded = Convert.ToInt32(Console.ReadLine());
+
+            //    while (eded > 0)
+            //    {
+            //        eded = eded / 10;
+            //        i++;
+
+            //    }
+            //    Console.WriteLine("Reqemlerin Sayi " + i);
+
+            //}
+            //findDigitOfNumberWithWhile();
+
+
+
+
+
+
+
+
+            //void findSumDigitOfNumber()
+            //{
+            //    Console.WriteLine("Istediyiniz Reqemi Yazin");
+            //    int screenNumber = Convert.ToInt32(Console.ReadLine());
+            //    int r;
+            //    int cem = 0;
+
+            //    while (screenNumber != 0)
+            //    {
+            //        r = screenNumber % 10;
+            //        screenNumber = screenNumber / 10;
+            //        cem = cem + r;
+            //    }
+            //    Console.WriteLine("Reqemlerin Cemi " + cem);
+            //}
+            //findSumDigitOfNumber();
+
+
+
+            //void showSentences()
+            //{
+            //    Console.WriteLine("Bir Cumle Yazin");
+            //    var cmle = Console.ReadLine();
+
+            //    string[] sozler = cmle.Split(' ');
+
+            //    foreach (var item in sozler)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            //showSentences();
+
+
+            //void noticeKeywordInStatement()
+            //    {
+            //        List<string> keywords = new List<string>() { "Tenbel", "Kod yazmayan", "Yola Veren", "Esneyen", "Bekar" };
+            //        Console.WriteLine("Cumle Yazin");
+            //        var cmle = Console.ReadLine();
+
+            //        foreach (var item in keywords)
+            //        {
+            //            if (cmle.Contains(item) == true)
+            //            {
+            //                Console.WriteLine("Siz Lozersiz");
+            //            }
+            //        }
+            //    }
+            //        noticeKeywordInStatement();
+
+
+            //void changeItemOfSentence()
+            //{
+            //    Console.WriteLine("Cumleni Yaz");
+            //    var cmle = Console.ReadLine();
+            //    string[] a = cmle.Split(' ');
+            //    var kw = "Loser";
+
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+
+
+            //        if (a[i] == "Tenbel" || a[i] == "tenbel")
+            //        {
+            //            a[i] = kw;
+            //        }
+            //        Console.Write(a[i] + " ");
+
+            //    }
+            //}
+            //changeItemOfSentence();
+
+
+            //void giveMoney()
+            //{
+            //    Console.WriteLine("Pul miqdarini daxil edin :");
+            //    var pul = Convert.ToInt32(Console.ReadLine());
+            //    int yuzduk = 0;
+            //    int elilik = 0;
+            //    int onlug = 0;
+            //    int birlik = 0;
+
+            //    yuzduk = pul / 100;
+            //    pul = pul % 100;
+            //    elilik = pul / 50;
+            //    pul = pul % 50;
+            //    onlug = pul / 10;
+            //    pul = pul % 10;
+            //    birlik = pul;
+
+            //    Console.WriteLine(yuzduk + " dene yuzduk " + elilik + " dene elilik " + onlug + " dene onlug " + birlik + " dene manatliq ");
+
+            //}
+
+            //giveMoney();
+
+
         }
+
     }
 }
+
---
+++ vovelConst.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vovel_Constant
+{
+    class vovelConst
+    {
+        List<char> sait = new List<char>();
+        List<char> samit = new List<char>();
+
+        public void Vowel(string value)
+        {
+
+            int vowel = 0;
+            int cont = 0;
+
+            foreach (var x in value.ToLower())
+            {
+                if (x.Equals('a') || x.Equals('e') || x.Equals('i') || x.Equals('o') || x.Equals('u'))
+                {
+                    vowel++;
+                    sait.Add(x);
+                }
+
+
+                if (x > 'a' && x <= 'd' || x > 'e' && x < 'i' || x > 'j' && x < 'o' || x >= 'p' && x < 'u' || x > 'v' && x < 'z')
+                {
+                    cont++;
+                    samit.Add(x);
+                }
+
+            }
+            
+            Console.WriteLine("sait Siyahisi");
+            Console.WriteLine();//bowluqlar ucun
+
+
+            foreach (var item in sait)
+            {
+
+                Console.Write(item + " ");
+                Console.WriteLine();
+
+            }
+
+
+
+            Console.WriteLine();//bowluqlar ucun
+            Console.WriteLine(vowel + " Eded Sait Var");
+            Console.WriteLine();
+
+            //saits end
+
+
+            Console.WriteLine("Samit Siyahisi");
+            Console.WriteLine();//bowluqlar ucun
+
+            foreach (var item in samit)
+            {
+
+                Console.Write(item + " ");
+                Console.WriteLine();//bowluqlar ucun
+
+            }
+
+            Console.WriteLine();//bowluqlar ucun
+
+            Console.WriteLine(cont + " Eded Samit Var");
+
+
+        }
+
+
+
+    }
+   
+}
